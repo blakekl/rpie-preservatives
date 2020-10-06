33ccfb1876e9712cd44944cf07202dbc158452ea
Blake Longmore <blongmore@ancestry.com>
2020-10-06 14:08:10 -0600

    Perform search and replace silently.

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ cp rpie-preservatives.sh /opt/retropie/configs/all/
 sudo chmod +x /opt/retropie/configs/all/rpie-preservatives.sh
 
 # replace any existing rpie-preservatives commands with the new one.
-sed 's/.\/rpie-preservatives.sh.*//' /opt/retropie/configs/all/runcommand-onend.sh 
-sed 's/.\/rpie-preservatives.sh.*//' /opt/retropie/configs/all/runcommand-onstart.sh 
+sed -in "s/.\/rpie-preservatives.sh.*//" /opt/retropie/configs/all/runcommand-onend.sh 
+sed -in "s/.\/rpie-preservatives.sh.*//" /opt/retropie/configs/all/runcommand-onstart.sh 
 
 echo "./rpie-preservatives.sh upload $1 $2 $3 $4" >> /opt/retropie/configs/all/runcommand-onend.sh
 echo "./rpie-preservatives.sh download $1 $2 $3 $4" >> /opt/retropie/configs/all/runcommand-onstart.sh
 
 # perform initial backup.
-/opt/retropie/configs/all/rpie-preservatives.sh upload'
+/opt/retropie/configs/all/rpie-preservatives.sh upload
